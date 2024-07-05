d091de88ec167e50d9b3000361c294ca5455fcac
Markus Muehlhaeusser <markus.muehlhaeusser@tum.de>
2024-07-05 22:21:04 +0200

    Clean up and add some comments

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-
+// Include the functor(s) to test
 #include <molecularDynamicsLibrary/KryptonPairFunctor.h>
 
 #include <molecularDynamicsLibrary/MoleculeLJ.h>
@@ -17,35 +17,22 @@ constexpr autopas::FunctorN3Modes functorN3Modes{autopas::FunctorN3Modes::Both};
 constexpr bool newton3{true};
 constexpr bool globals{true};
 
+// Chose the Functor
 using Functor = mdLib::KryptonPairFunctor<Particle, false, functorN3Modes, globals>;
 
-void printHelp() {
-    std::cout << "Usage: \n"
-        << "./AutoPasFunctorBench <functor type> <repetitions> <iterations> <numParticles> <hit rate> <outfile>\n"
-        << "Possible values:\n"
-        << "functor type: \"aos\", \"single\", \"pair\", \"verlet\"\n"
-        << "repetitions, iterations, numParticles: int\n"
-        << "hit rate: double\n"
-        << "outfile: std::string"
-        << std::endl;
-}
-
 /**
- * Mini benchmark tool to estimate the inner most kernel performance of AutoPas
+ * Mini validation setup that writes functor results to a CSV
  * @return
  */
 int main(int argc, char* argv[]) {
 
     const double cutoff{300.};
     Functor functor{cutoff};
-//    std::vector<Particle> {
-//        Particle({0., 0., 0.}, {0., 0., 0.}, 0, 0),
-//        Particle({0.3, 0., 0.}, {0., 0., 0.}, 1, 0)
-//    };
+
     auto particle1 = Particle({0., 0., 0.}, {0., 0., 0.}, 0, 0);
     auto particle2 = Particle({0., 0., 0.}, {0., 0., 0.}, 0, 0);
 
-            // Open the file
+    // Open the file
     std::ofstream file("kryptonPair2.csv");
 
     // Check if the file is opened successfully
@@ -62,23 +49,26 @@ int main(int argc, char* argv[]) {
     size_t resolution = 1000;
     double increment = 0.7 / resolution;
     double distance = 1.10798137169225 * 0.35;
-    while (distance < 15.51) {
-        epot = 0.0;
+    for (auto i = 0; i < resolution; i++) {
+        // move second particle along x-Axis
         particle2.setF({0.,0.,0.});
         particle2.setR({distance, 0., 0.});
+        // Compute the interactions
         functor.initTraversal();
         functor.AoSFunctor(particle1, particle2, newton3);
         functor.endTraversal(newton3);
+        // Results
         auto force = particle1.getF();
         epot = functor.getPotentialEnergy();
         virial = functor.getVirial();
+
         // std::cout << "Distance,   " << distance << std::endl;
         // std::cout << "Force,      " << force[0] << std::endl;
         // std::cout << "Potential Energy:" << epot << std::endl;
 
+        // Write to CSV
         file << std::fixed << std::setprecision(15) << distance << "," << force[0] << "," << epot << "," << virial << std::endl;
-        distance += 0.1;
-        break;
+        distance += increment;
     }
 
     file.close();
