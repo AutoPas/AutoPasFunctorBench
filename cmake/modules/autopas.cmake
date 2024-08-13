127022af8fcad5bc9033f243d730c9a31c424be5
Markus Muehlhaeusser <markus.muehlhaeusser@tum.de>
2024-08-13 18:37:38 +0200

    Updates for the 3B-Krypton Functor

--- cmake/modules/autopas.cmake
+++ cmake/modules/autopas.cmake
@@ -13,7 +13,7 @@ endif ()
 FetchContent_Declare(
         autopasfetch
         GIT_REPOSITORY ${autopasRepoPath}
-        GIT_TAG AbInitioArgonPotential    # Name of the branch with functor to test
+        GIT_TAG Krypton3bFunctor    # Name of the branch with functor to test
 )
 # Populate dependency
 FetchContent_MakeAvailable(autopasfetch)
--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 // Include the functor(s) to test
 #include <molecularDynamicsLibrary/AxilrodTellerFunctor.h>
+#include <molecularDynamicsLibrary/KryptonExtendedATMFunctor.h>
 
 #include <molecularDynamicsLibrary/MoleculeLJ.h>
 #include <autopas/cells/FullParticleCell.h>
@@ -18,7 +19,24 @@ constexpr bool newton3{true};
 constexpr bool globals{true};
 
 // Chose the Functor
-using Functor = mdLib::AxilrodTellerFunctor<Particle, false, functorN3Modes, globals>;
+using Functor = mdLib::KryptonExtendedATMFunctor<Particle, functorN3Modes, globals>;
+//using Functor = mdLib::AxilrodTellerFunctor<Particle, false, functorN3Modes, globals>;
+
+void setEquilateral(Particle &p2, Particle &p3, double distance) {
+    p2.setR({0., distance, 0.});
+    p3.setR({std::sqrt(3)/2 * distance, 0.5 * distance, 0.});
+}
+
+void setLinear(Particle &p2, Particle &p3, double distance) {
+    p2.setR({distance, 0., 0.});
+    p3.setR({2 * distance, 0., 0.});
+}
+
+void setLinearP1(Particle &p1, Particle &p2, Particle &p3, double distance) {
+    p1.setR({10. - distance, 0., 0.});
+    p2.setR({10., 0., 0.});
+    p3.setR({10.2, 0., 0.});
+}
 
 /**
  * Mini validation setup that writes functor results to a CSV
@@ -28,14 +46,15 @@ int main(int argc, char* argv[]) {
 
     const double cutoff{300.};
     Functor functor{cutoff};
-    functor.setParticleProperties(1.6152500E-3);
+
+//    functor.setParticleProperties(1.61525e-3);
 
     auto particle1 = Particle({0., 0., 0.}, {0., 0., 0.}, 0, 0);
     auto particle2 = Particle({0., 0., 0.}, {0., 0., 0.}, 0, 0);
     auto particle3 = Particle({0., 0., 0.}, {0., 0., 0.}, 0, 0);
 
     // Open the file
-    std::ofstream file("AxilrodTeller/EquilateralGeometry.csv");
+    std::ofstream file("kryptonTripleLinear.csv");
 
     // Check if the file is opened successfully
     if (!file.is_open()) {
@@ -44,23 +63,28 @@ int main(int argc, char* argv[]) {
     }
 
     // Write headers
-    file << "Distance,Energy,Force_x,Force_y,Force_z" << std::endl;
+    file << "Distance [nm],Energy [K],Force_x [K/nm],Force_y [K/nm],Force_z [K/nm]" << std::endl;
 
     double epot{};
     size_t resolution = 1000;
     double increment = 0.001;
-    double distance = 0.3;
+    double distance = 0.25;
     for (auto i = 0; i < resolution; i++) {
-        // move second particle
+        // Reset forces
+        particle1.setF({0.,0.,0.});
         particle2.setF({0.,0.,0.});
-        particle2.setR({0., distance, 0.});
-        // move third particle
         particle3.setF({0.,0.,0.});
-        particle3.setR({std::sqrt(3)/2 * distance, 0.5 * distance, 0.});
+
+        // Move particles
+//        setEquilateral(particle2, particle3, distance);
+//        setLinear(particle2, particle3, distance);
+        setLinearP1(particle1, particle2, particle3, distance);
+
         // Compute the interactions
         functor.initTraversal();
         functor.AoSFunctor(particle1, particle2, particle3, newton3);
         functor.endTraversal(newton3);
+
         // Results
         auto force = particle1.getF();
         epot = functor.getPotentialEnergy();
