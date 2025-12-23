50b01b73f35f76c2df36e4e8de9300a4fe96f7f4
Markus Muehlhaeusser <markus.muehlhaeusser@tum.de>
2025-12-23 17:49:09 +0100

    Refactor for google benchmark

--- main.cpp
+++ main.cpp
@@ -5,257 +5,261 @@
 #include "benchmark/benchmark.h"
 
 #include <autopas/cells/FullParticleCell.h>
-#include <autopas/utils/Timer.h>
-#include <fstream>
 #include <random>
 
 // type aliases for ease of use
 using Particle = mdLib::MoleculeLJ;
 using Cell = autopas::FullParticleCell<Particle>;
 
-// some constants that define the benchmark
-constexpr bool mixing{false};
-constexpr autopas::FunctorN3Modes functorN3Modes{autopas::FunctorN3Modes::Both};
-constexpr bool newton3{true};
-constexpr bool globals{false};
-
-// using ATM = mdLib::AxilrodTellerFunctor<Particle, false, functorN3Modes, globals>;
-using ATM = mdLib::AxilrodTellerMutoFunctor<Particle, false, functorN3Modes, globals>;
-
-enum FunctorMode {
+enum FunctorMode
+{
     AOS,
     SOASINGLE,
     SOAPAIR,
     SOATRIPLE
 };
 
-double distSquared(std::array<double, 3> a, std::array<double, 3> b) {
+double distSquared(std::array<double, 3> a, std::array<double, 3> b)
+{
     using autopas::utils::ArrayMath::sub;
     using autopas::utils::ArrayMath::dot;
-    const auto c = sub(a, b);   // 3 FLOPS
-    return dot(c, c);           // 3+2=5 FLOPs
+    const auto c = sub(a, b); // 3 FLOPS
+    return dot(c, c); // 3+2=5 FLOPs
 }
 
-void generateParticles(ATM &functor, std::vector<Cell> &cells, const size_t numberOfParticles,
-    const double cellSize, const FunctorMode mode){
+template <typename FunctorType>
+void generateParticles(FunctorType& functor, std::vector<Cell>& cells, const size_t numberOfParticles,
+                       const double cellSize, const FunctorMode mode)
+{
     // generate randomly distributed particles
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_real_distribution<double> dis(0.0, cellSize);
 
-    auto fillCellWithParticles = [&] (Cell& cell, double xShift, double yShift, double zShift) {
-        for (size_t particleId = 0; particleId < numberOfParticles; ++particleId) {
+    auto fillCellWithParticles = [&](Cell& cell, double xShift, double yShift, double zShift)
+    {
+        for (size_t particleId = 0; particleId < numberOfParticles; ++particleId)
+        {
             Particle p{
-                { dis(gen) + xShift, dis(gen) + yShift, dis(gen)},
+                {dis(gen) + xShift, dis(gen) + yShift, dis(gen)},
                 {0., 0., 0.,},
                 particleId,
-                0};
+                0
+            };
             cell.addParticle(p); // Add particle to the current cell
         }
     };
-
-    switch (mode) {
-        case AOS:
+    switch (mode)
+    {
+    case AOS:
         fillCellWithParticles(cells[0], 0., 0., 0.);
-            break;
-        case SOATRIPLE:
-            fillCellWithParticles(cells[2], 0., cellSize, 0.);
-            functor.SoALoader(cells[2], cells[2]._particleSoABuffer, 0, false);
-        case SOAPAIR:
-            fillCellWithParticles(cells[1], cellSize, 0., 0.);
-            functor.SoALoader(cells[1], cells[1]._particleSoABuffer, 0, false);
-        case SOASINGLE:
-            fillCellWithParticles(cells[0], 0., 0., 0.);
-            functor.SoALoader(cells[0], cells[0]._particleSoABuffer, 0, false);
-            break;
+        break;
+    case SOATRIPLE:
+        fillCellWithParticles(cells[2], 0., cellSize, 0.);
+        functor.SoALoader(cells[2], cells[2]._particleSoABuffer, 0, false);
+    case SOAPAIR:
+        fillCellWithParticles(cells[1], cellSize, 0., 0.);
+        functor.SoALoader(cells[1], cells[1]._particleSoABuffer, 0, false);
+    case SOASINGLE:
+        fillCellWithParticles(cells[0], 0., 0., 0.);
+        functor.SoALoader(cells[0], cells[0]._particleSoABuffer, 0, false);
+        break;
     }
 }
 
-void applyAoSFunctor(ATM &functor, Cell &cell) {
-    for(std::size_t i = 0; i < cell.size(); ++i) {
-        for (std::size_t j = i + 1; j < cell.size(); ++j) {
-            for (std::size_t k = j + 1; k < cell.size(); ++k) {
-                // timer.at("AoSFunctor on Particles").start();
+template <typename FunctorType>
+void applyAoSFunctor(FunctorType& functor, Cell& cell, bool newton3)
+{
+    for (std::size_t i = 0; i < cell.size(); ++i)
+    {
+        for (std::size_t j = i + 1; j < cell.size(); ++j)
+        {
+            for (std::size_t k = j + 1; k < cell.size(); ++k)
+            {
                 functor.AoSFunctor(cell[i], cell[j], cell[k], newton3);
-                // timer.at("AoSFunctor on Particles").stop();
             }
         }
     }
 }
 
-void applyFunctorOnParticles(ATM &functor, std::vector<Cell> &cells, const FunctorMode mode) {
-    switch (mode) {
-        case AOS:
-            applyAoSFunctor(functor, cells[0]);
-            break;
-        case SOASINGLE:
-            functor.SoAFunctorSingle(cells[0]._particleSoABuffer, newton3);
-            break;
-        case SOAPAIR:
-            functor.SoAFunctorPair(cells[0]._particleSoABuffer, cells[1]._particleSoABuffer, newton3);
-            break;
-        case SOATRIPLE:
-            functor.SoAFunctorTriple(cells[0]._particleSoABuffer, cells[1]._particleSoABuffer, cells[2]._particleSoABuffer, newton3);
-            break;
+template <typename FunctorType>
+void applyFunctorOnParticles(FunctorType& functor, std::vector<Cell>& cells, const FunctorMode mode, bool newton3)
+{
+    switch (mode)
+    {
+    case AOS:
+        applyAoSFunctor(functor, cells[0], newton3);
+        break;
+    case SOASINGLE:
+        functor.SoAFunctorSingle(cells[0]._particleSoABuffer, newton3);
+        break;
+    case SOAPAIR:
+        functor.SoAFunctorPair(cells[0]._particleSoABuffer, cells[1]._particleSoABuffer, newton3);
+        break;
+    case SOATRIPLE:
+        functor.SoAFunctorTriple(cells[0]._particleSoABuffer, cells[1]._particleSoABuffer, cells[2]._particleSoABuffer,
+                                 newton3);
+        break;
     }
 }
 
-// void csvOutput(ATM &functor, std::vector<Cell> &cells) {
-//     timer.at("Output").start();
-//     std::ofstream csvFile("particles.csv");
-//     if (not csvFile.is_open()) {
-//         throw std::runtime_error("FILE NOT OPEN!");
-//     }
-//     for (size_t cellId = 0; cellId < cells.size(); ++cellId) {
-//         functor.SoAExtractor(cells[cellId], cells[cellId]._particleSoABuffer, 0);
-//         csvFile << "ParticleId,rX,rY,rZ,fX,fY,fZ\n";
-//         for (auto p : cells[0]) {
-//             using autopas::utils::ArrayUtils::to_string;
-//             csvFile << p.getID() << ","
-//                     << to_string(p.getR(), ",", {"", ""}) << ","
-//                     << to_string(p.getF(), ",", {"", ""})
-//                     << "\n";
-//         }
-//         csvFile.close();
-//         timer.at("Output").stop();
-//     }
-// }
-
-std::tuple<size_t, size_t> countInteractions(std::vector<Cell> &cells, const double cutoff, const FunctorMode mode) {
+std::tuple<size_t, size_t> countInteractions(std::vector<Cell>& cells, const double cutoff, const FunctorMode mode)
+{
     size_t calcsDist{0};
     size_t calcsForce{0};
     const auto cutoffSquared{cutoff * cutoff};
 
-    switch (mode) {
-        case AOS:
-        case SOASINGLE:
-            for (size_t i = 0; i < cells[0].size(); i++) {
-                for (size_t j = i + 1; j < cells[0].size(); j++) {
-                    if (distSquared(cells[0][i].getR(), cells[0][j].getR()) > cutoffSquared) continue;
-                    for (size_t k = j + 1; k < cells[0].size(); k++) {
-                        if (distSquared(cells[0][i].getR(), cells[0][k].getR()) > cutoffSquared) continue;
-                        if (distSquared(cells[0][j].getR(), cells[0][k].getR()) > cutoffSquared) continue;
-                        ++calcsForce;
-                    }
+    switch (mode)
+    {
+    case AOS:
+    case SOASINGLE:
+        for (size_t i = 0; i < cells[0].size(); i++)
+        {
+            for (size_t j = i + 1; j < cells[0].size(); j++)
+            {
+                if (distSquared(cells[0][i].getR(), cells[0][j].getR()) > cutoffSquared) continue;
+                for (size_t k = j + 1; k < cells[0].size(); k++)
+                {
+                    if (distSquared(cells[0][i].getR(), cells[0][k].getR()) > cutoffSquared) continue;
+                    if (distSquared(cells[0][j].getR(), cells[0][k].getR()) > cutoffSquared) continue;
+                    ++calcsForce;
                 }
             }
-            calcsDist = cells[0].size() * (cells[0].size() - 1) * (cells[0].size() - 2) / 6;
-            break;
-        case SOAPAIR:
-            for (size_t i = 0; i < cells[0].size(); i++) {
-                for (size_t j = i + 1; j < cells[0].size(); j++) {
-                    if (distSquared(cells[0][i].getR(), cells[0][j].getR()) > cutoffSquared) continue;
-                    for (size_t k = 0; k < cells[1].size(); k++) {
-                        if (distSquared(cells[0][i].getR(), cells[1][k].getR()) > cutoffSquared) continue;
-                        if (distSquared(cells[0][j].getR(), cells[1][k].getR()) > cutoffSquared) continue;
-                        ++calcsForce;
-                    }
-                }
-                for (size_t j = 0; j < cells[1].size(); j++) {
-                    if (distSquared(cells[0][i].getR(), cells[1][j].getR()) > cutoffSquared) continue;
-                    for (size_t k = j + 1; k < cells[1].size(); k++) {
-                        if (distSquared(cells[0][i].getR(), cells[1][k].getR()) > cutoffSquared) continue;
-                        if (distSquared(cells[1][j].getR(), cells[1][k].getR()) > cutoffSquared) continue;
-                        ++calcsForce;
-                    }
+        }
+        calcsDist = cells[0].size() * (cells[0].size() - 1) * (cells[0].size() - 2) / 6;
+        break;
+    case SOAPAIR:
+        for (size_t i = 0; i < cells[0].size(); i++)
+        {
+            for (size_t j = i + 1; j < cells[0].size(); j++)
+            {
+                if (distSquared(cells[0][i].getR(), cells[0][j].getR()) > cutoffSquared) continue;
+                for (size_t k = 0; k < cells[1].size(); k++)
+                {
+                    if (distSquared(cells[0][i].getR(), cells[1][k].getR()) > cutoffSquared) continue;
+                    if (distSquared(cells[0][j].getR(), cells[1][k].getR()) > cutoffSquared) continue;
+                    ++calcsForce;
                 }
             }
-            calcsDist = cells[0].size() * cells[1].size() * (cells[0].size() + cells[1].size() - 2) / 2;
-            break;
-        case SOATRIPLE:
-            for (size_t i = 0; i < cells[0].size(); i++) {
-                for (size_t j = 0; j < cells[1].size(); j++) {
-                    if (distSquared(cells[0][i].getR(), cells[1][j].getR()) > cutoffSquared) continue;
-                    for (size_t k = 0; k < cells[2].size(); k++) {
-                        if (distSquared(cells[0][i].getR(), cells[2][k].getR()) > cutoffSquared) continue;
-                        if (distSquared(cells[1][j].getR(), cells[2][k].getR()) > cutoffSquared) continue;
-                        ++calcsForce;
-                    }
+            for (size_t j = 0; j < cells[1].size(); j++)
+            {
+                if (distSquared(cells[0][i].getR(), cells[1][j].getR()) > cutoffSquared) continue;
+                for (size_t k = j + 1; k < cells[1].size(); k++)
+                {
+                    if (distSquared(cells[0][i].getR(), cells[1][k].getR()) > cutoffSquared) continue;
+                    if (distSquared(cells[1][j].getR(), cells[1][k].getR()) > cutoffSquared) continue;
+                    ++calcsForce;
                 }
             }
-            calcsDist = cells[0].size() * cells[1].size() * cells[2].size();
-            break;
-        default:
-            break;
+        }
+        calcsDist = cells[0].size() * cells[1].size() * (cells[0].size() + cells[1].size() - 2) / 2;
+        break;
+    case SOATRIPLE:
+        for (size_t i = 0; i < cells[0].size(); i++)
+        {
+            for (size_t j = 0; j < cells[1].size(); j++)
+            {
+                if (distSquared(cells[0][i].getR(), cells[1][j].getR()) > cutoffSquared) continue;
+                for (size_t k = 0; k < cells[2].size(); k++)
+                {
+                    if (distSquared(cells[0][i].getR(), cells[2][k].getR()) > cutoffSquared) continue;
+                    if (distSquared(cells[1][j].getR(), cells[2][k].getR()) > cutoffSquared) continue;
+                    ++calcsForce;
+                }
+            }
+        }
+        calcsDist = cells[0].size() * cells[1].size() * cells[2].size();
+        break;
+    default:
+        break;
     }
     return {calcsDist, calcsForce};
 }
 
-class FunctorTestFixture : public benchmark::Fixture
+
+template <typename FunctorType>
+static void BM_Functor(benchmark::State& state, FunctorMode functorMode, bool newton3)
 {
-public:
-    void SetUp(const benchmark::State&) override
-    {
-
-    }
-    void TearDown(const benchmark::State&) override
-    {
-
-    }
-
-    const double cellSize{3.};
-    const double cutoff{3.};
+    const auto numParticles = static_cast<size_t>(state.range(0));
+    const auto cellSize = static_cast<double>(state.range(1));
+    const auto cutoff = static_cast<double>(state.range(2));
     const double nu{1.0};
 
-    const size_t numParticles{100};
-    const size_t iterations{1};
-    const std::array<FunctorMode, 4> functorsToTest = {
-        AOS,
-        SOASINGLE,
-        SOAPAIR,
-        SOATRIPLE
+    FunctorType functor{cutoff};
+    functor.setParticleProperties(nu);
+
+    std::size_t calcsDistTotal = 0;
+    std::size_t calcsForceTotal = 0;
+
+    for (auto _ : state)
+    {
+        state.PauseTiming();
+        std::vector<Cell> cells{3};
+        generateParticles(functor, cells, numParticles, cellSize, functorMode);
+        state.ResumeTiming();
+
+        applyFunctorOnParticles(functor, cells, functorMode, newton3);
+
+        state.PauseTiming();
+        const auto [calcsDist, calcsForce] = countInteractions(cells, cutoff, functorMode);
+        calcsDistTotal += calcsDist;
+        calcsForceTotal += calcsForce;
+        state.ResumeTiming();
+    }
+    // Per-iteration averages and hit rate as user counters.
+    const auto iters = static_cast<double>(state.iterations());
+    const double avgDist = static_cast<double>(calcsDistTotal) / iters;
+    const double avgForce = static_cast<double>(calcsForceTotal) / iters;
+    const double hitRate = avgForce / avgDist * 100.0;
+
+    using benchmark::Counter;
+    auto roundToPrecision = [](double x, unsigned int precision)
+    {
+        return std::round(x * std::pow(10, precision)) / std::pow(10, precision);
+    };
+    state.counters["HitRate [%]"] = roundToPrecision(hitRate, 2);
+    state.counters["Time per Triplet"] = Counter(avgDist, Counter::kIsRate | Counter::kInvert, Counter::OneK::kIs1000);
+    state.counters["Time per Interaction"] = Counter(avgForce, Counter::kIsRate | Counter::kInvert,
+                                                     Counter::OneK::kIs1000);;
+    state.counters["# of Triplets"] = avgDist;
+}
+
+template <typename FunctorType>
+void registerOneBenchmark(const std::string& functorName, const std::string& modeName, FunctorMode functorMode,
+                          bool newton3)
+{
+    benchmark::RegisterBenchmark(
+            "BM_" + functorName + "_" + modeName,
+            [=](benchmark::State& state) { BM_Functor<FunctorType>(state, functorMode, newton3); })
+        ->RangeMultiplier(2)->Ranges({{4, 32}, {3, 3}, {3, 3}});
+}
+
+
+void RegisterFunctorBenchmarks()
+{
+    // some constants that define the benchmark
+    constexpr bool mixing{false};
+    constexpr autopas::FunctorN3Modes functorN3Modes{autopas::FunctorN3Modes::Both};
+    constexpr bool globals{false};
+    constexpr bool newton3{true};
+
+    using ATM = mdLib::AxilrodTellerMutoFunctor<Particle, mixing, functorN3Modes, globals>;
+    using ATM2 = mdLib::AxilrodTellerMutoFunctor<Particle, mixing, functorN3Modes, true>;
+
+    const std::array modes = {
+        std::make_pair("AoS", AOS),
+        std::make_pair("SoASingle", SOASINGLE),
+        std::make_pair("SoAPair", SOAPAIR),
+        std::make_pair("SoATriple", SOATRIPLE)
     };
 
-    void runBenchmarkForFunctorMode(benchmark::State& state, FunctorMode functorMode) const
+    for (const auto& [modeName, mode] : modes)
     {
-        ATM functor{cutoff};
-        functor.setParticleProperties(nu);
-
-        std::size_t calcsDistTotal = 0;
-        std::size_t calcsForceTotal = 0;
-
-        for (auto _ : state)
-        {
-            state.PauseTiming();
-            std::vector<Cell> cells{3};
-            generateParticles(functor, cells, numParticles, cellSize, functorMode);
-            state.ResumeTiming();
-
-            applyFunctorOnParticles(functor, cells, functorMode);
-
-            state.PauseTiming();
-            const auto [calcsDist, calcsForce] = countInteractions(cells, cutoff, functorMode);
-            calcsDistTotal += calcsDist;
-            calcsForceTotal += calcsForce;
-            state.ResumeTiming();
-        }
-        // Per-iteration averages and hit rate as user counters.
-        const double iters = static_cast<double>(state.iterations());
-        const double avgDist = static_cast<double>(calcsDistTotal) / iters;
-        const double avgForce = static_cast<double>(calcsForceTotal) / iters;
-        const double hitRate = avgForce / avgDist * 100.0;
-
-        using benchmark::Counter;
-        state.counters["HitRate [%]"] = hitRate;
-        state.counters["Time per Triplet"] = Counter(avgDist, Counter::kIsRate | Counter::kInvert, Counter::OneK::kIs1000);
-        state.counters["Time per Interaction"] = Counter(avgForce, Counter::kIsRate | Counter::kInvert, Counter::OneK::kIs1000);;
+        registerOneBenchmark<ATM>("ATM", modeName, mode, newton3);
+        registerOneBenchmark<ATM2>("ATM2", modeName, mode, newton3);
     }
-};
-
-BENCHMARK_F(FunctorTestFixture, AoS)(benchmark::State& state) {
-    runBenchmarkForFunctorMode(state, FunctorMode::AOS);
 }
 
-BENCHMARK_F(FunctorTestFixture, SoASingle)(benchmark::State& state) {
-    runBenchmarkForFunctorMode(state, FunctorMode::SOASINGLE);
-}
-
-BENCHMARK_F(FunctorTestFixture, SoAPair)(benchmark::State& state) {
-    runBenchmarkForFunctorMode(state, FunctorMode::SOAPAIR);
-}
-
-BENCHMARK_F(FunctorTestFixture, SoATriple)(benchmark::State& state) {
-    runBenchmarkForFunctorMode(state, FunctorMode::SOATRIPLE);
-}
-
+// before BENCHMARK_MAIN()
+static bool registerAll = (RegisterFunctorBenchmarks(), true);
 BENCHMARK_MAIN();
